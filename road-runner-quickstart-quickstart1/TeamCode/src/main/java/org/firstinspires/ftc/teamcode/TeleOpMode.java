87c70f26ca417421033eef0d4004a39dece8953c
Leon0423 <liying3040@gmail.com>
2023-12-31 12:48:54 +0800

    add new usage

--- road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -13,34 +13,29 @@ public class TeleOpMode extends LinearOpMode {
 
     private DcMotor FR, FL, BR, BL;
     private double drive, turn, strafe;
-    private double drive_speed;
+    double drive_speed;
     private double drive_speed_max = 0.7;
     private double fr, fl, br, bl, scale;
+    private double boxy, boxx;
+    private double rightstickx, leftstick;
+    private double max;
 
     @Override
     public void runOpMode() throws InterruptedException {
-
         init_hardware();
-
         while(!isStarted()) {
-
             init_telemetry();
-
         }
-
         waitForStart();
-
         while(opModeIsActive()){
-
             init_telemetry();
-
             teleOpControls();
-
+            chassis_power();
         }
-
     }
 
     public void init_hardware(){
+
         FR=hardwareMap.get(DcMotorEx.class, "FR");
         FL=hardwareMap.get(DcMotorEx.class, "FL");
         BR=hardwareMap.get(DcMotorEx.class, "BR");
@@ -58,6 +53,7 @@ public class TeleOpMode extends LinearOpMode {
         BL.setZeroPowerBehavior(BRAKE);
         FR.setZeroPowerBehavior(BRAKE);
         BR.setZeroPowerBehavior(BRAKE);
+
     }
 
     public void teleOpControls(){
@@ -66,12 +62,23 @@ public class TeleOpMode extends LinearOpMode {
         turn = gamepad1.right_stick_x;      //自旋
         strafe = -gamepad1.left_stick_x;    //平移
 
-        //gamepad caculate
-        double boxy = gamepad1.left_stick_y;
-        double boxx = gamepad1.left_stick_x;
-        double rightstickx = Math.abs (gamepad1.right_stick_x);
-        double leftstick = Math.abs (Math.sqrt (Math.pow (boxy, 2) + Math.pow (boxx, 2)));
+        boxy = gamepad1.left_stick_y;
+        boxx = gamepad1.left_stick_x;
+        rightstickx = Math.abs (gamepad1.right_stick_x);
+        leftstick = Math.abs (Math.sqrt (Math.pow (boxy, 2) + Math.pow (boxx, 2)));
 
+    }
+
+    public double scaling_power(double fr, double fl, double br, double bl){
+
+        max = Math.max( Math.max( Math.abs(fr), Math.abs(fl) ), Math.max( Math.abs(br), Math.abs(bl) ));
+        if (max <= 1){
+            max = 1;
+        }
+        return max;
+    }
+
+    public double drive_speed(double rightstickx, double leftstick){
         if(leftstick < 0.2){
             drive_speed = 0;
         } else if(leftstick < 0.5){
@@ -79,7 +86,6 @@ public class TeleOpMode extends LinearOpMode {
         } else{
             drive_speed = drive_speed_max;
         }
-
         if(rightstickx < 0.2){
             drive_speed = 0;
         } else if(rightstickx < 0.5) {
@@ -87,25 +93,21 @@ public class TeleOpMode extends LinearOpMode {
         } else if(rightstickx < 1){
             drive_speed = drive_speed_max;
         }
+        return drive_speed;
+    }
 
-        fr = (- drive - strafe + turn) * drive_speed;
-        fl = (- drive + strafe - turn) * drive_speed;
-        br = (- drive + strafe + turn) * drive_speed;
-        bl = (- drive - strafe - turn) * drive_speed;
+    public void chassis_power(){
+
+        fr = (- drive - strafe + turn) * drive_speed(rightstickx, leftstick);
+        fl = (- drive + strafe - turn) * drive_speed(rightstickx, leftstick);
+        br = (- drive + strafe + turn) * drive_speed(rightstickx, leftstick);
+        bl = (- drive - strafe - turn) * drive_speed(rightstickx, leftstick);
 
         scale = scaling_power(fr, fl, br, bl);
         FR.setPower(fr / scale);
         FL.setPower(fl / scale);
         BR.setPower(br / scale);
         BL.setPower(bl / scale);
-    }
-
-    public double scaling_power(double fr, double fl, double br, double bl){
-        double max = Math.max( Math.max( Math.abs(fr), Math.abs(fl) ), Math.max( Math.abs(br), Math.abs(bl) ));
-        if (max <= 1){
-            max = 1;
-        }
-        return max;
     }
 
     public void init_telemetry(){
